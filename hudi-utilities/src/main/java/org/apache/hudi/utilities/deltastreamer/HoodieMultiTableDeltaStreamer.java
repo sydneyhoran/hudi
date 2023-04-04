0da2d90a30be398613bc28764c40b7ead6cd9c9f
Sydney Horan <sydney.horan@penn-interactive.com>
2023-04-04 18:25:52 -0400

    support for parsing S3 target directory as db/schema/tablename for multitable tables that have db.schema.tablename

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/deltastreamer/HoodieMultiTableDeltaStreamer.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/deltastreamer/HoodieMultiTableDeltaStreamer.java
@@ -115,7 +115,8 @@ public class HoodieMultiTableDeltaStreamer {
     for (String table : tablesToBeIngested) {
       String[] tableWithDatabase = table.split("\\.");
       String database = tableWithDatabase.length > 1 ? tableWithDatabase[0] : "default";
-      String currentTable = tableWithDatabase.length > 1 ? tableWithDatabase[1] : table;
+      String currentTable = tableWithDatabase.length > 1 ? (tableWithDatabase.length == 3 ? tableWithDatabase[1] + "." + tableWithDatabase[2] : tableWithDatabase[1]) : table;
+      logger.info("Current table is " + currentTable);
       String configProp = HoodieDeltaStreamerConfig.INGESTION_PREFIX + database + Constants.DELIMITER + currentTable + Constants.INGESTION_CONFIG_SUFFIX;
       String configFilePath = properties.getString(configProp, Helpers.getDefaultConfigFilePath(configFolder, database, currentTable));
       checkIfTableConfigFileExists(configFolder, fs, configFilePath);
@@ -421,7 +422,11 @@ public class HoodieMultiTableDeltaStreamer {
   private static String resetTarget(Config configuration, String database, String tableName) {
     String basePathPrefix = configuration.basePathPrefix;
     basePathPrefix = basePathPrefix.charAt(basePathPrefix.length() - 1) == '/' ? basePathPrefix.substring(0, basePathPrefix.length() - 1) : basePathPrefix;
-    String targetBasePath = basePathPrefix + Constants.FILE_DELIMITER + database + Constants.FILE_DELIMITER + tableName;
+    String basePathWithDatabase = basePathPrefix + Constants.FILE_DELIMITER + database;
+    String[] tableNameWithSchema = tableName.split("\\.");
+    String targetBasePath = tableNameWithSchema.length > 1 ? basePathWithDatabase + Constants.FILE_DELIMITER
+        + tableNameWithSchema[0] + Constants.FILE_DELIMITER + tableNameWithSchema[1]
+        : basePathWithDatabase + Constants.FILE_DELIMITER + tableName;
     configuration.targetTableName = database + Constants.DELIMITER + tableName;
     return targetBasePath;
   }
